c70181d0ee9318b4b5aa78862deb923ec4a368d1
Brandon <bsmithcompsci@gmail.com>
2021-04-24 00:35:54 -0700

    Init

---
+++ module.ts
@@ -0,0 +1,14 @@
+import { Module, ModuleManager } from "../../API/Module";
+import fs from "fs";
+import path from "path";
+
+class BaseModule extends Module
+{
+    constructor()
+    {
+        super("Official Generic Layout", fs.readFileSync(path.resolve(__dirname, "./version.txt")).toString("utf-8"));
+        this.RegisterAssetFolder(__dirname + '/assets');
+    }
+}
+
+ModuleManager.RegisterModule(new BaseModule());
